1c462d3a7c769979cefbaf7673336421139d357f
Nabil Ridhwanshah <57290483+nabilridhwan@users.noreply.github.com>
2019-11-09 15:16:19 +0800

    Merging with Master from Future (#1)
    * Changed bot interval in demo credentials from 1s to 30s
    
    * Refactored for new_twitter_bot to __TwitterBot__ for TwitterBot init
    
    * New rule
    
    * Added new rule
    
    * Removed rule

--- .~c9_invoke_36yI1.py
+++ .~c9_invoke_36yI1.py
@@ -4,20 +4,20 @@ from credentials import *
 from time import sleep
 from TwitterBot import TwitterBot
 
-# Tweepy initialisation "http://docs.tweepy.org/en/v3.5.0/getting_started.html"
+# Tweepy initialisation
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
 
-__TwitterBot__ = TwitterBot(0, api)
+new_twitter_bot = TwitterBot(0, api)
 
 # Main Function (invoking below)
 def main():
-    __TwitterBot__ = TwitterBot(0, api)
+    new_twitter_bot = TwitterBot(0, api)
 
 if __name__ == "__main__":
     # TODO: note that it deletes in interval of 200 tweets
-    __TwitterBot__.destroy_all_beta_tweets()
+    new_twitter_bot.destroy_all_beta_tweets()
 
     # while True:
     #     main()
